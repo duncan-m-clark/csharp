dccb1b2dc9ae3472d33849ab5e6104f8e75efb03
Duncan <126290637+duncan-m-clark@users.noreply.github.com>
2025-04-02 10:25:11 -0400

    learning more about main

--- AboutMain/aboutMain.cs
+++ AboutMain/aboutMain.cs
@@ -1,4 +1,4 @@
-class AboutMain {
+class aboutMain {
     /*Main: entry point of program
             must be declared inside class or struct
             enclosing class can be static
@@ -14,3 +14,21 @@ class AboutMain {
         Console.WriteLine(args.Length);
     }
 }
+
+/*Use "dotnet new console" to create a new .NET project
+  class wrapping Main must be named identically to the .cs file
+  "dotnet build" will build the program
+  create a powershell script to run the program (similar to makefile):
+
+  dotnet run
+  if ($LastExitCode -eq 0) {
+    Write-Host "Execution succeeded"
+  } else {
+    Write-Host "Execution failed"
+  }
+  Write-Host "Return value = " $LastExistCode
+
+  powershell script file extension = .ps1
+  type name of file with script in the powershell to run script
+
+  dotnet run -- <insert> <arguments> <here>*/
