abe33d29793ddb936e8e250cf76a4d5a94d4588a
Duncan <126290637+duncan-m-clark@users.noreply.github.com>
2025-04-02 10:42:41 -0400

    learning about top-level statements

---
+++ TopLevelStatements/Program.cs
@@ -0,0 +1,23 @@
+Console.WriteLine("Hello World");
+
+/*The above is a complete C# program. Top-level
+  statements are statements not wrapped by a class
+  or method
+  
+  Projects may only have one top-level file,
+  because it serves as an entry point to the program
+  (similar to Main())
+
+  Projects with top-level file entry points will ignore
+  all Main() entry points. The -main compiler option
+  will can not be used with top-level files, even if there
+  are multiple Main() methods
+
+  using directives must still come before top-level statements
+
+  Top-level files may still have namespaces and type
+  definitions, but they must come after the top-level
+  statements
+
+  You can still return an exit code by including a return
+  statement at after the top-level statements*/
