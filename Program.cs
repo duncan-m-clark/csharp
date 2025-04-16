9b52b297a60762b7eb2786d4407ce4e35cf2a075
Duncan <126290637+duncan-m-clark@users.noreply.github.com>
2025-04-16 12:32:55 +0000

    implemented  functionality to let user play multiple rounds

--- Program.cs
+++ Program.cs
@@ -1,48 +1,67 @@
 ﻿public class Game {
     static void Main() {
         Console.WriteLine("Welcome to the Number Guessing Game!");
-        Console.WriteLine("I'm thinking of a number between 1 and 100.");
-        Console.WriteLine("\nSelect your difficulty level:");
-        Console.WriteLine("1. Easy (10 chances)");
-        Console.WriteLine("2. Medium (5 chances)");
-        Console.WriteLine("3. Hard (3 chances)\n");
-        Console.Write("Enter your choice: ");
-        
-        int option = Convert.ToInt32(Console.ReadLine());
-        int target = new Random().Next(1, 101);
-        string difficulty = "";
-        int tries = 0;
 
-        switch (option) {
-            case 1:
-                difficulty = "Easy";
-                tries = 10;
-                break;
-            case 2:
-                difficulty = "Medium";
-                tries = 5;
-                break;
-            case 3:
-                difficulty = "Hard";
-                tries = 3;
-                break;
-        }
+        bool quit = false;
+        while (!quit) {
+            Console.WriteLine("I'm thinking of a number between 1 and 100.");
+            Console.WriteLine("\nSelect your difficulty level:");
+            Console.WriteLine("1. Easy (10 chances)");
+            Console.WriteLine("2. Medium (5 chances)");
+            Console.WriteLine("3. Hard (3 chances)\n");
+            Console.Write("Enter your choice: ");
+            
+            int option = Convert.ToInt32(Console.ReadLine());
+            int target = new Random().Next(1, 101);
+            string difficulty = "";
+            int tries = 0;
 
-        Console.WriteLine($"Great! You have selected the {difficulty} difficulty level.");
-        Console.WriteLine("Let's start the game!\n");
-
-        for (int i = 0; i < tries; i++) {
-            Console.Write("Enter your guess: ");
-            int guess = Convert.ToInt32(Console.ReadLine());
-
-            if (guess < target) {
-                Console.WriteLine($"Incorrect! The number is greater than {guess}.\n");
-            } else if (guess > target) {
-                Console.WriteLine($"Incorrect! The number is less than {guess}.\n");
-            } else {
-                Console.WriteLine($"Congratulations! You guessed the corect number in {i+1} attempts.");
-                break;
+            switch (option) {
+                case 1:
+                    difficulty = "Easy";
+                    tries = 10;
+                    break;
+                case 2:
+                    difficulty = "Medium";
+                    tries = 5;
+                    break;
+                case 3:
+                    difficulty = "Hard";
+                    tries = 3;
+                    break;
             }
+
+            Console.WriteLine($"Great! You have selected the {difficulty} difficulty level.");
+            Console.WriteLine("Let's start the game!\n");
+            bool guessedIt = false;
+
+            for (int i = 0; i < tries; i++) {
+                Console.Write("Enter your guess: ");
+                int guess = Convert.ToInt32(Console.ReadLine());
+
+                if (guess < target) {
+                    Console.WriteLine($"Incorrect! The number is greater than {guess}.\n");
+                } else if (guess > target) {
+                    Console.WriteLine($"Incorrect! The number is less than {guess}.\n");
+                } else {
+                    Console.WriteLine($"Congratulations! You guessed the corect number in {i+1} attempts.");
+                    guessedIt = true;
+                    break;
+                }
+            }
+
+            if (!guessedIt) {
+                Console.WriteLine("Oh no! You've failed to guess the number.");
+            }
+
+            Console.Write("Would you like to play again? (Y/N)");
+            char cont = Convert.ToChar(Console.ReadLine());
+
+            if (cont.Equals('N')) {
+                Console.WriteLine("Thanks for playing!");
+                quit = true;
+            }
+
         }
     }
 }
